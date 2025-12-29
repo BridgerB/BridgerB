e52e5e66b08d2c6d811ef1706742a668711091ca
BridgerB <git@bridgerb.com>
2025-12-28 21:54:15 -0700

    feat: google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { page } from '$app/state';
 	import Navbar from '$lib/Navbar.svelte';
 	import 'the-new-css-reset/css/reset.css';
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import 'devicon/devicon.min.css';
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.png';
+	import { initAnalytics, trackPageView } from '$lib/firebase/analytics';
+
+	const PRODUCTION_DOMAIN = 'bridgerb.com';
 
 	let { children } = $props();
-	
+	let analyticsReady = $state(false);
+
+	onMount(() => {
+		if (page.url.hostname === PRODUCTION_DOMAIN) {
+			initAnalytics().then(() => {
+				analyticsReady = true;
+			});
+		}
+	});
+
+	$effect(() => {
+		if (analyticsReady && page.url) {
+			trackPageView(page.url.pathname, document.title);
+		}
+	});
 </script>
 
 <svelte:head>
